691abad5cc68ad37810a68afeb8855670708687a
Nikita <fomin.nv@phystech.edu>
2024-09-04 00:10:10 +0300

    buttons.py

---
+++ buttons.py
@@ -0,0 +1,26 @@
+from aiogram.filters.command import Command
+from aiogram import types, Dispatcher
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from main import dp
+import utils
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    # Создаем сборщика клавиатур типа Reply
+    builder = ReplyKeyboardBuilder()
+    # Добавляем в сборщик одну кнопку
+    builder.add(types.KeyboardButton(text="Начать игру"))
+    # Прикрепляем кнопки к сообщению
+    await message.answer("Добро пожаловать в квиз!", reply_markup=builder.as_markup(resize_keyboard=True))
+
+from aiogram import F
+
+# Хэндлер на команды /quiz
+@dp.message(F.text=="Начать игру")
+@dp.message(Command("quiz"))
+async def cmd_quiz(message: types.Message):
+    # Отправляем новое сообщение без кнопок
+    await message.answer(f"Давайте начнем квиз!")
+    # Запускаем новый квиз
+    await utils.new_quiz(message)
