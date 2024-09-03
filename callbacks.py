d3c671652c8e4c713cae20583184575aa32154f1
Nikita <fomin.nv@phystech.edu>
2024-09-04 00:21:31 +0300

    callbacks.py

---
+++ callbacks.py
@@ -0,0 +1,62 @@
+from main import dp
+from aiogram import types, F
+from quiz_data import quiz_data
+import database as db
+import utils
+
+@dp.callback_query(F.data == "right_answer")
+async def right_answer(callback: types.CallbackQuery):
+    # редактируем текущее сообщение с целью убрать кнопки (reply_markup=None)
+    await callback.bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None
+    )
+
+    # Получение текущего вопроса для данного пользователя
+    current_question_index = await db.get_quiz_index(callback.from_user.id)
+
+    # Отправляем в чат сообщение, что ответ верный
+    await callback.message.answer("Верно!")
+
+    # Обновление номера текущего вопроса в базе данных
+    current_question_index += 1
+    await db.update_quiz_index(callback.from_user.id, current_question_index)
+
+    # Проверяем достигнут ли конец квиза
+    if current_question_index < len(quiz_data):
+        # Следующий вопрос
+        await utils.get_question(callback.message, callback.from_user.id)
+    else:
+        # Уведомление об окончании квиза
+        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
+
+
+@dp.callback_query(F.data == "wrong_answer")
+async def wrong_answer(callback: types.CallbackQuery):
+    # редактируем текущее сообщение с целью убрать кнопки (reply_markup=None)
+    await callback.bot.edit_message_reply_markup(
+        chat_id=callback.from_user.id,
+        message_id=callback.message.message_id,
+        reply_markup=None
+    )
+
+    # Получение текущего вопроса для данного пользователя
+    current_question_index = await db.get_quiz_index(callback.from_user.id)
+
+    correct_option = quiz_data[current_question_index]['correct_option']
+
+    # Отправляем в чат сообщение об ошибке с указанием верного ответа
+    await callback.message.answer(f"Неправильно. Правильный ответ: {quiz_data[current_question_index]['options'][correct_option]}")
+
+    # Обновление номера текущего вопроса в базе данных
+    current_question_index += 1
+    await db.update_quiz_index(callback.from_user.id, current_question_index)
+
+    # Проверяем достигнут ли конец квиза
+    if current_question_index < len(quiz_data):
+        # Следующий вопрос
+        await utils.get_question(callback.message, callback.from_user.id)
+    else:
+        # Уведомление об окончании квиза
+        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
